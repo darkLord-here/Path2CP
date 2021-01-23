32349ca58f1f12e159380e0fe3070328ab306740
Shailesh <stupidnspooky.lord@gmail.com>
2021-01-23 21:43:05 +0530

    Bubble Sort Implemented and Corrected Output

--- Searching and Sorting/bubbleSort.cpp
+++ Searching and Sorting/bubbleSort.cpp
@@ -1,6 +1,6 @@
-//Selection Sort 
+//Bubble Sort 
 /*
-We start from the first element in an array and then compare with all the elements
+We start from the first element in an array and then compare with adjacent element
 if it is greater then we swap else we continue to the next element and repeat the same process.
 */
 
@@ -21,11 +21,11 @@ void swap(int *a, int *b){
     *a = *a - *b;
 }
 
-void selectionSort(int *arr, int n){
+void bubbleSort(int *arr, int n){
     int j;
-    for(int i=0; i<n-1; i++){
-        for(int j=i+1; j<n; j++){
-            if(arr[j]<arr[i]) swap(arr[j],arr[i]); 
+    for(int i=0; i<n; i++){
+        for(int j=0; j<i; j++){
+            if(arr[j]>arr[j+1]) swap(arr[j],arr[j+1]); 
         }
         cout<<"Step "<<i<<" : ";
         printContent(arr,n);
@@ -36,8 +36,8 @@ int main(){
     int array[20] = {10,9,8,7,6,5,4,3,2,1,11,2,3,4,5,6,7,8,9,10};
     cout<<"[Input Array]:\t";
     printContent(array,20);
-    cout<<"[Running Selection Sort]"<<endl;
-    selectionSort(array,20);
+    cout<<"[Running Bubble Sort]"<<endl;
+    bubbleSort(array,20);
     cout<<"[Output Array]:\t";
     printContent(array,20);
     return 0;
